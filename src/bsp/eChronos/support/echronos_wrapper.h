8e1fcba1e2711360efcacaf84e5fdba3443abfe0
Ihor Kuz <ihor.kuz@nicta.com.au>
2014-02-26 17:49:09 +1100

    Moved internal echronos_wrapper.h from include to support.

--- src/bsp/eChronos/support/echronos_wrapper.h
+++ src/bsp/eChronos/support/echronos_wrapper.h
@@ -8,10 +8,18 @@
 #ifndef ECHRONOS_WRAPPER_H_
 #define ECHRONOS_WRAPPER_H_
 
+#include <assert.h>
+
+//#define pdTRUE          1
+//#define pdFALSE         0
+
 //eChronos specific
 
 //#define taskYIELD()		rtos_yield()
 
+#define UNIMPLEMENTED() assert(!"UNIMPLEMENETED");
+#define vNOP() UNIMPLEMENTED()
+#define NOP(type) do { UNIMPLEMENTED(); return (type)0; } while(0)
 
 
 
