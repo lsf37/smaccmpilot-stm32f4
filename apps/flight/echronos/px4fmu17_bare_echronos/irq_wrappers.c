12377780aebb71a1a62de4d809ab3365f546cb99
Robert Sison <Robert.Sison@nicta.com.au>
2014-11-05 18:39:15 +1100

    Replay all ioar_echronos changes since pre-April 2014 for bare_echronos

--- apps/flight/echronos/px4fmu17_bare_echronos/irq_wrappers.c
+++ apps/flight/echronos/px4fmu17_bare_echronos/irq_wrappers.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <ctype.h>
 
+#include <assert.h>
+
 #include <eChronos.h>
 #include <rtos-kochab.h>
 
@@ -19,9 +21,42 @@
             }                                             \
         }
 
-
-
-
+#define IRQ_WRAPPER_DEFER_SEM(x) \
+        extern void *x##_IRQHandler(void);                \
+        static void *x##_sem;                             \
+        static int x##_sem_give_count;                    \
+        bool eChronos_##x##_IRQHandler(void)              \
+        {                                                 \
+            void *sem;                                    \
+            sem = x##_IRQHandler();                       \
+            if (sem) {                                    \
+                if (x##_sem_give_count) {                 \
+                    assert(sem == x##_sem); /* XXX */     \
+                } else {                                  \
+                    x##_sem = sem;                        \
+                }                                         \
+                x##_sem_give_count++;                     \
+                rtos_irq_event_raise(IRQ_EVENT_ID_##x);   \
+                return true;                              \
+            }                                             \
+            return false;                                 \
+        }                                                 \
+        void x##_IRQHandler_wrapper(void)                 \
+        {                                                 \
+            while (1) {                                   \
+                int i, give_count;                        \
+                void *give_sem;                           \
+                rtos_signal_wait_set(SIGNAL_SET_IRQ_##x); \
+                ulPortSetInterruptMask();                 \
+                give_count = x##_sem_give_count;          \
+                give_sem = x##_sem;                       \
+                x##_sem_give_count = 0;                   \
+                vPortClearInterruptMask(0);               \
+                for (i = 0; i < give_count; i++) {        \
+                    xSemaphoreGive(give_sem);             \
+                }                                         \
+            }                                             \
+        }
 
 #define IRQ_WRAPPER(x) \
         bool eChronos_##x##_IRQHandler(void)              \
@@ -41,12 +76,67 @@
         }
 
 
-IRQ_WRAPPER_NAKED(I2C2_EV)
-IRQ_WRAPPER_NAKED(I2C2_ER)
-IRQ_WRAPPER_NAKED(SPI1)
-IRQ_WRAPPER_NAKED(UART5)
+IRQ_WRAPPER_DEFER_SEM(I2C2_EV)
+IRQ_WRAPPER_DEFER_SEM(I2C2_ER)
+IRQ_WRAPPER_DEFER_SEM(SPI1)
+//IRQ_WRAPPER_NAKED(UART5)
 IRQ_WRAPPER_NAKED(USART1)
 // IRQ_WRAPPER_NAKED(USART2)
 IRQ_WRAPPER_NAKED(USART6)
 IRQ_WRAPPER_NAKED(TIM1_UP_TIM10)
 IRQ_WRAPPER_NAKED(TIM1_CC)
+
+int ivory_isr_sem_count = 0;
+void *ivory_isr_sem_handle = NULL;
+
+extern void UART5_IRQHandler(void);
+static void *UART5_sem;
+static int UART5_sem_give_count;
+
+bool eChronos_UART5_IRQHandler(void)
+{
+    assert(ivory_isr_sem_count == 0);
+    assert(ivory_isr_sem_handle == NULL);
+
+    UART5_IRQHandler();
+
+    if (ivory_isr_sem_count > 1) {
+        assert(ivory_isr_sem_count <= 1); /* XXX */
+    }
+
+    if (ivory_isr_sem_count) {
+        if (UART5_sem_give_count) {
+            assert(ivory_isr_sem_handle == UART5_sem); /* XXX */
+        } else {
+            UART5_sem = ivory_isr_sem_handle;
+        }
+        ivory_isr_sem_handle = NULL;
+        UART5_sem_give_count += ivory_isr_sem_count;
+        ivory_isr_sem_count = 0;
+        rtos_irq_event_raise(IRQ_EVENT_ID_UART5);
+        return true;
+    }
+
+    return false;
+}
+
+/* NB: this is just the same as the version in DEFER_SEM macro */
+void UART5_IRQHandler_wrapper(void)
+{
+    while (1) {
+        int i, give_count;
+        void *give_sem;
+
+        rtos_signal_wait_set(SIGNAL_SET_IRQ_UART5);
+
+        ulPortSetInterruptMask();
+        give_count = UART5_sem_give_count;
+        give_sem = UART5_sem;
+        UART5_sem_give_count = 0;
+        vPortClearInterruptMask(0);
+
+        for (i = 0; i < give_count; i++) {
+            xSemaphoreGive(give_sem);
+        }
+    }
+}
