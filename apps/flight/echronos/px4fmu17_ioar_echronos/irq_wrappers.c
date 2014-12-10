64df193d75a0f0488a2ecbe54c337a286cd0cab1
Robert Sison <Robert.Sison@nicta.com.au>
2014-12-10 17:42:07 +1100

    sandbox-smaccm: Substitute USART1 for UART5 for hard connection GCS testing and add helpful comments

--- apps/flight/echronos/px4fmu17_ioar_echronos/irq_wrappers.c
+++ apps/flight/echronos/px4fmu17_ioar_echronos/irq_wrappers.c
@@ -21,6 +21,12 @@
             }                                             \
         }
 
+/* If an interrupt handler intends to post to a semaphore, it must return the
+ * handle of that semaphore, otherwise NULL.
+ * The assertion below tests the assumption that no x_IRQHandler() can return
+ * two different semaphore handles on subsequent calls before a previous call
+ * has been processed by the x_IRQHandler_wrapper() thread. */
+
 #define IRQ_WRAPPER_DEFER_SEM(x) \
         extern void *x##_IRQHandler(void);                \
         static void *x##_sem;                             \
@@ -31,7 +37,7 @@
             sem = x##_IRQHandler();                       \
             if (sem) {                                    \
                 if (x##_sem_give_count) {                 \
-                    assert(sem == x##_sem); /* XXX */     \
+                    assert(sem == x##_sem);               \
                 } else {                                  \
                     x##_sem = sem;                        \
                 }                                         \
@@ -78,8 +84,8 @@
 IRQ_WRAPPER_DEFER_SEM(I2C2_EV)
 IRQ_WRAPPER_DEFER_SEM(I2C2_ER)
 IRQ_WRAPPER_DEFER_SEM(SPI1)
-//IRQ_WRAPPER_NAKED(UART5)
-IRQ_WRAPPER_NAKED(USART1)
+IRQ_WRAPPER_NAKED(UART5)
+//IRQ_WRAPPER_NAKED(USART1)
 IRQ_WRAPPER_NAKED(USART2)
 IRQ_WRAPPER_NAKED(USART6)
 IRQ_WRAPPER_NAKED(TIM1_UP_TIM10)
@@ -88,31 +94,36 @@ IRQ_WRAPPER_NAKED(TIM1_CC)
 int ivory_isr_sem_count = 0;
 void *ivory_isr_sem_handle = NULL;
 
-extern void UART5_IRQHandler(void);
-static void *UART5_sem;
-static int UART5_sem_give_count;
+extern void USART1_IRQHandler(void);
+static void *USART1_sem;
+static int USART1_sem_give_count;
 
-bool eChronos_UART5_IRQHandler(void)
+bool eChronos_USART1_IRQHandler(void)
 {
     assert(ivory_isr_sem_count == 0);
     assert(ivory_isr_sem_handle == NULL);
 
-    UART5_IRQHandler();
+    USART1_IRQHandler();
 
-    if (ivory_isr_sem_count > 1) {
-        assert(ivory_isr_sem_count <= 1); /* XXX */
-    }
+    /* This tests the theory that it just so happens that xSemaphoreGiveFromISR
+     * is never called twice in a single run of the USART1 interrupt handler. */
+    assert(ivory_isr_sem_count <= 1);
 
     if (ivory_isr_sem_count) {
-        if (UART5_sem_give_count) {
-            assert(ivory_isr_sem_handle == UART5_sem); /* XXX */
+        if (USART1_sem_give_count) {
+            /* Assumes:
+             * - USART1 is the only interrupt whose handler originates calls to
+             *     ivory_echronos_semaphore_give_isr.
+             * - ivory_echronos_semaphore_give_isr is the only caller of
+             *     xSemaphoreGiveFromISR. */
+            assert(ivory_isr_sem_handle == USART1_sem);
         } else {
-            UART5_sem = ivory_isr_sem_handle;
+            USART1_sem = ivory_isr_sem_handle;
         }
         ivory_isr_sem_handle = NULL;
-        UART5_sem_give_count += ivory_isr_sem_count;
+        USART1_sem_give_count += ivory_isr_sem_count;
         ivory_isr_sem_count = 0;
-        rtos_interrupt_event_raise(RTOS_INTERRUPT_EVENT_ID_UART5);
+        rtos_interrupt_event_raise(RTOS_INTERRUPT_EVENT_ID_USART1);
         return true;
     }
 
@@ -120,7 +131,7 @@ bool eChronos_UART5_IRQHandler(void)
 }
 
 /* NB: this is just the same as the version in DEFER_SEM macro */
-void UART5_IRQHandler_wrapper(void)
+void USART1_IRQHandler_wrapper(void)
 {
     while (1) {
         int i, give_count;
@@ -129,9 +140,9 @@ void UART5_IRQHandler_wrapper(void)
         rtos_signal_wait(RTOS_SIGNAL_ID_GENERIC);
 
         ulPortSetInterruptMask();
-        give_count = UART5_sem_give_count;
-        give_sem = UART5_sem;
-        UART5_sem_give_count = 0;
+        give_count = USART1_sem_give_count;
+        give_sem = USART1_sem;
+        USART1_sem_give_count = 0;
         vPortClearInterruptMask(0);
 
         for (i = 0; i < give_count; i++) {
